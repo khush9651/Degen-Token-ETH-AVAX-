5237cf600837441446290098a1ed9b4eab1ea271
Khushi Vishwakarma <155286143+khush9651@users.noreply.github.com>
2024-06-29 19:32:23 +0530

    DegenToken.sol

--- DegenToken.sol
+++ DegenToken.sol
@@ -1,1 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract DegenToken is ERC20, Pausable, Ownable {
+    event TokensRedeemed(address indexed user, uint256 amount, string note);
+
+    constructor() ERC20("Degen", "DGN") Ownable(msg.sender) {
+        _mint(msg.sender, 1000000 * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) external onlyOwner whenNotPaused {
+        _mint(to, amount);
+    }
+
+    function redeem(uint256 amount, string calldata note) external whenNotPaused {
+        require(balanceOf(msg.sender) >= amount, "Insufficient balance to redeem");
+        _burn(msg.sender, amount);
+        emit TokensRedeemed(msg.sender, amount, note);
+        // Implement additional redemption logic here, e.g., rewarding in-game items
+    }
+
+    function burn(uint256 amount) external whenNotPaused {
+        _burn(msg.sender, amount);
+    }
+
+    function pause() external onlyOwner {
+        _pause();
+    }
+
+    function unpause() external onlyOwner {
+        _unpause();
+    }
+}
 
